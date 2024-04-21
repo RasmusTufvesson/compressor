f760962c9309141e04d19f21a32159ebdbb2d9ef
RasmusTufvesson <lars@bizzbook.com>
2024-04-21 18:39:37 +0200

    add gif support

--- readme.md
+++ readme.md
@@ -2,6 +2,11 @@
 This is a very simple program that takes an image and converts it to a jpg with a specified quality.
 
 ## How to use
-This is a command line program so call it from there. There are two arguments, the path to an image and the target output quality. The output quality is a number between 1 and 100. When it is done the program will output to a file called `out.png`. A call could look like this:
+This is a command line program so call it from there. There are two arguments, the path to an image and the target output quality. The output quality is a number between 1 and 100 where the higher the number the higher the quality. When it is done the program will output to a file called `out.jpg`. You can also provide the program with a gif file and a third argument and the program will compress the gif and output it into a file called `out.gif`. The third argument is supposed to be between 1 and 30 where the lower the number the higher the quality.
+
+Here are a few example calls:
 
 `compressor.exe "C:/path/to/image.png" 10`
+`compressor.exe "C:/path/to/image.png" 50`
+`compressor.exe "C:/path/to/gif.gif" 100 30`
+`compressor.exe "C:/path/to/gif.gif" 1 1`
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use std::{env::args, fs::File};
-use image::{codecs::jpeg::JpegEncoder, io::Reader};
+use std::{env::args, fs::File, io::{BufReader, Cursor, Write}, sync::{Arc, Mutex}};
+use image::{codecs::{gif::{GifDecoder, GifEncoder}, jpeg::JpegEncoder}, io::Reader, AnimationDecoder, DynamicImage, Frame, ImageBuffer, Rgb};
+use indicatif::{ParallelProgressIterator, ProgressBar, ProgressStyle};
+use rayon::iter::{IndexedParallelIterator, IntoParallelRefIterator, ParallelIterator};
 
 fn main() {
     let mut args = args().skip(1);
@@ -8,8 +10,50 @@ fn main() {
     if quality < 1 || quality > 100 {
         panic!("Quality needs to be between 1 and 100");
     }
-    let img = Reader::open(path).expect("Could not open file").decode().expect("Could not decode file").to_rgb8();
-    let mut out_file = File::create("out.jpg").expect("Could not create out file");
-    let encoder = JpegEncoder::new_with_quality(&mut out_file, quality);
+    if path.ends_with(".gif") {
+        let gif_quality: i32 = args.next().expect("Supply target gif quality as third command line argument").parse().expect("Could not parse target gif quality");
+        if gif_quality < 1 || gif_quality > 30 {
+            panic!("Gif quality needs to be between 1 and 30");
+        }
+        let file_in = BufReader::new(File::open(path).unwrap());
+        let decoder = GifDecoder::new(file_in).unwrap();
+        let frames = decoder.into_frames();
+        let frames = frames.collect_frames().expect("Error decoding gif");
+        let mut out_file = File::create("out.gif").expect("Could not create out file");
+        encode_gif(frames, &mut out_file, quality, gif_quality);
+    } else {
+        let img = Reader::open(path).expect("Could not open file").decode().expect("Could not decode file").to_rgb8();
+        let mut out_file = File::create("out.jpg").expect("Could not create out file");
+        encode(img, &mut out_file, quality);
+    }
+}
+
+fn encode(img: ImageBuffer<Rgb<u8>, Vec<u8>>, buffer: &mut impl Write, quality: u8) {
+    let encoder = JpegEncoder::new_with_quality(buffer, quality);
     img.write_with_encoder(encoder).expect("Could not encode");
+}
+
+fn encode_gif(frames: Vec<Frame>, buffer: &mut impl Write, jpg_quality: u8, gif_quality: i32) {
+    let progress_style = ProgressStyle::with_template("{msg} {wide_bar}").unwrap();
+    let out_frames = if jpg_quality != 100 {
+        let out_frames: Vec<Option<Frame>> = vec![None; frames.len()];
+        let out_frames = Arc::new(Mutex::new(out_frames));
+        frames.par_iter().enumerate().progress_with(ProgressBar::new(frames.len() as u64).with_style(progress_style.clone()).with_message("Compressing")).for_each(|(i, frame)| {
+            let original_buffer: ImageBuffer<Rgb<u8>, Vec<u8>> = DynamicImage::from(frame.clone().into_buffer()).into();
+            let mut frame_buffer = vec![];
+            encode(original_buffer, &mut frame_buffer, jpg_quality);
+            out_frames.lock().unwrap()[i] = Some(Frame::from_parts(Reader::with_format(Cursor::new(frame_buffer), image::ImageFormat::Jpeg).decode().unwrap().into(), frame.left(), frame.top(), frame.delay()));
+        });
+        let out_frames = out_frames.lock().unwrap().clone();
+        out_frames
+    } else {
+        frames.iter().map(|x| Some(x.clone())).collect()
+    };
+    let mut encoder = GifEncoder::new_with_speed(buffer, gif_quality);
+    encoder.set_repeat(image::codecs::gif::Repeat::Infinite).unwrap();
+    let progress = ProgressBar::new(frames.len() as u64).with_style(progress_style).with_message("Encoding gif");
+    for frame in out_frames {
+        progress.inc(1);
+        encoder.encode_frame(frame.unwrap()).expect("Could not encode");
+    }
 }
